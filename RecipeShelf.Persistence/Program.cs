b2faf84a5a0fab0d0797ed0978b80a5e067b023e
Sri Prasad Tadimalla <prasad_tad@yahoo.com>
2017-02-26 21:14:22 +1000

    support for new recipes and ingredients

--- RecipeShelf.Persistence/Program.cs
+++ RecipeShelf.Persistence/Program.cs
@@ -10,6 +10,7 @@ using System;
 using System.Threading.Tasks;
 using RecipeShelf.Data.Proxies;
 using RecipeShelf.Data.Server.Proxies;
+using System.Security.Cryptography;
 
 namespace RecipeShelf.Persistence
 {
@@ -68,6 +69,7 @@ namespace RecipeShelf.Persistence
             if (!await fileProxy.CanConnectAsync() ||
                 !await noSqlProxy.CanConnectAsync() ||
                 !ingredientCache.CanConnect()) return false;
+            if (ingredient.Id == null) ingredient.Id = GetNewId();
             await fileProxy.PutTextAsync($"ingredients/{ingredient.Id}.json", JsonConvert.SerializeObject(ingredient, Formatting.Indented));
             await noSqlProxy.PutIngredientAsync(ingredient);
             ingredientCache.Store(ingredient);
@@ -80,11 +82,39 @@ namespace RecipeShelf.Persistence
                 !await noSqlProxy.CanConnectAsync() ||
                 !recipeCache.CanConnect() ||
                 !markdownProxy.CanConnect()) return false;
+            if (recipe.Id == null) recipe.Id = GetNewId();
             await fileProxy.PutTextAsync($"recipes/{recipe.Id}.json", JsonConvert.SerializeObject(recipe, Formatting.Indented));
             await noSqlProxy.PutRecipeAsync(recipe);
             recipeCache.Store(recipe);
             await markdownProxy.PutRecipeMarkdownAsync(recipe);
             return true;
+        }
+
+        private static string GetNewId()
+        {
+            var length = 8;
+            var numBytes = (int)(Math.Log(Math.Pow(64, length)) / Math.Log(2) / 8);
+            return Convert.ToBase64String(GenerateRandomBytes(numBytes)).Replace('/', '_').Replace('+', '-').Replace("=", string.Empty).Substring(0, length);
+        }
+
+        private static byte[] GenerateRandomBytes(int length)
+        {
+            // Create a buffer
+            byte[] randBytes;
+
+            if (length >= 1)
+                randBytes = new byte[length];
+            else
+                randBytes = new byte[1];
+
+            // Create a new RNGCryptoServiceProvider.
+            var rand = RandomNumberGenerator.Create();
+
+            // Fill the buffer with random bytes.
+            rand.GetBytes(randBytes);
+
+            // return the bytes.
+            return randBytes;
         }
     }
 }
