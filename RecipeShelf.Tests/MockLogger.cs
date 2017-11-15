2ac498e783b78aec750f835e38a49c9551751694
Sri Prasad Tadimalla <prasad_tad@yahoo.com>
2017-11-15 23:20:03 +1100

    Added delay so GetText test passes, mock logger logs to console with colors

--- RecipeShelf.Tests/MockLogger.cs
+++ RecipeShelf.Tests/MockLogger.cs
@@ -24,6 +24,30 @@ namespace RecipeShelf.Tests
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
+            var oldColor = Console.ForegroundColor;
+            switch (logLevel)
+            {
+                case LogLevel.Critical:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogLevel.Debug:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case LogLevel.Information:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case LogLevel.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogLevel.Trace:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+            }
+            Console.WriteLine($"{typeof(T).Name}: {formatter(state,exception)}");
+            Console.ForegroundColor = oldColor;
         }
     }
 }
